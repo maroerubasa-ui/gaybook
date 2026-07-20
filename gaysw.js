d2243e69f322f2e6c71b99d09a61880dfac93ee1
maroerubasa-ui <maroerubasa@gmail.com>
2026-07-20 23:14:35 +0100

    Update gaysw.js

--- gaysw.js
+++ gaysw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gaybook-v2'; // Incrementing version to force update
+const CACHE_NAME = 'gaybook-v212'; // Incrementing version to force update
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
